7727c612856e9905fe7ba6147e46274d2a67de44
Ábel Kalamár <abel.kalamar@gmail.com>
2019-03-25 17:57:14 +0100

    create routing to main page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { RegisterComponent } from './register/register.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   {path: 'login', component: LoginPageComponent},
-  {path: 'register', component: RegisterComponent}
+  {path: 'register', component: RegisterComponent},
+  {path: 'main', component: NavbarComponent}
 ];
 
 @NgModule({
--- frontend/src/services/register.service.ts
+++ frontend/src/services/register.service.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterService {
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private router: Router
+    ) { }
 
-  register(userData: { username: string, password: string, email: string }) {
+  register(userData: { username: string, password: string, email: string }): void {
     const headers: HttpHeaders = new HttpHeaders()
       .set('Content-Type', 'application/json');
     this.http.post<Object>('http://10.27.6.60:8080/api/user/register', {
@@ -21,18 +25,20 @@ export class RegisterService {
       });
   }
 
-  login(userData: { username: string, password: string }) {
-    const headers: HttpHeaders = new HttpHeaders()
-      .set('Content-Type', 'application/json')
-      .set('X-Requested-With', 'XMLHttpRequest');
-    this.http.post<Object>('http://10.27.6.60:8080/api/auth/login', {
-      username: userData.username,
-      password: userData.password
-    }, { headers })
-      .subscribe((data: { token: string }) => {
-        window.localStorage.setItem('token', data.token);
-        console.log(data);
-      });
+  login(userData: { username: string, password: string }): void {
+    // const headers: HttpHeaders = new HttpHeaders()
+    //   .set('Content-Type', 'application/json')
+    //   .set('X-Requested-With', 'XMLHttpRequest');
+    // this.http.post<Object>('http://10.27.6.60:8080/api/auth/login', {
+    //   username: userData.username,
+    //   password: userData.password
+    // }, { headers })
+    //   .subscribe((data: { token: string }) => {
+    //     window.localStorage.setItem('token', data.token);
+    //     console.log(data);
+    //     this.router.navigate(['/main']);
+    //   });
+    this.router.navigate(['/main']);
   }
 
   // auth() {
